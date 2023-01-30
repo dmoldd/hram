05a1aaff4507afc110a41c9b05632b3c30632365
Dmoldd <oldtime_tula@mail.ru>
2023-01-30 17:39:12 +0300

    Исправил глюк со скриптом

--- source/js/video-catalog.js
+++ source/js/video-catalog.js
@@ -25,7 +25,7 @@ function setupVideo(video) {
 }
 
 function parseMediaURL(media) {
-  let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+  let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)/i;
   let url = media.src;
   let match = url.match(regexp);
 
